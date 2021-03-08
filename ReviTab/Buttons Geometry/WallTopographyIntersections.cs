0029e34a84f13dcc9d551b88c38da375a7567ca6
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2021-03-09 07:06:55 +1100

    fix namespace

--- ReviTab/Buttons Geometry/WallTopographyIntersections.cs
+++ ReviTab/Buttons Geometry/WallTopographyIntersections.cs
@@ -1,13 +1,10 @@
 ﻿#region Namespaces
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
-using rg = Rhino.Geometry;
 
 #endregion
 
